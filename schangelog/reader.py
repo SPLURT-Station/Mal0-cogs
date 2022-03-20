35275d10c03079864d812e7356cec323ef8ade06
BongaTheProto <93835010+BongaTheProto@users.noreply.github.com>
2022-03-19 22:10:03 -0500

    new cl system, new cog
    I guess it was going to happen sooner or later

--- schangelog/reader.py
+++ schangelog/reader.py
@@ -1,36 +1,56 @@
-import yaml, os, glob
+import yaml, os, datetime, pprint
 
-def reader(instancePath):
+def change_to_tuples(dict):
     """
-    Used for reading and organizing the yaml files
+    Turns an unitary dictionary into a tuple
     """
-    if not os.path.exists(instancePath):
-        return
-    clPath = os.path.join(os.path.abspath(instancePath), "Repository/html/changelogs")
-    currentCl = {}
-    numCl = 0
-    for fileName in glob.glob(os.path.join(clPath, "*.yml")):
-        name = os.path.splitext(os.path.basename(fileName))
-        if name[0].startswith('.'): continue
-        if name[0] == 'example': continue
-        cl = {}
-        numCl += 1
-        with open(fileName, 'r', encoding='utf-8') as f:
-            cl = yaml.load(f, Loader=yaml.SafeLoader)
-        changes = cl.get('changes')
-        newChanges = {}
-        for ch in changes:
-            for k, v in ch.items():
-                if k in newChanges:
-                    newChanges[k] += [v]
-                else:
-                    newChanges.update({k: [v]})
-        if cl.get('author') in currentCl:
-            for k, v in newChanges.items():
-                if k in currentCl.get(cl.get('author')):
-                    currentCl[cl.get('author')].update({k: currentCl.get(cl.get('author')).get(k) + v})
-                else:
-                    currentCl[cl.get('author')].update({k: v})
-        else:
-            currentCl.update({cl['author']: newChanges})
-    return (numCl, currentCl)
+    return (list(dict.keys())[0], list(dict.values())[0])
+
+class RepoError(Exception):
+    pass
+
+def readCl(instancePath, dateOverride = datetime.date.today().strftime("%Y-%m-%d")):
+    """
+    Used for reading and organizing the yaml Changelog Files
+    """
+    dateOverride = datetime.datetime.strptime(dateOverride, '%Y-%m-%d')
+    archiveDir = os.path.join(os.path.abspath(instancePath), "Repository/html/changelogs/archive")
+    if not os.path.exists(archiveDir):
+        raise RepoError("There was a problem while browsing the repository")
+    formattedDate = dateOverride.strftime("%Y-%m")
+    monthPath = os.path.join(archiveDir, formattedDate + ".yml")
+    currentEntries = {}
+    if not os.path.exists(monthPath):
+        raise AttributeError
+    with open(monthPath, 'r', encoding='utf-8') as f:
+        currentEntries = yaml.load(f, Loader=yaml.SafeLoader)
+    return createChanges(currentEntries, dateOverride)
+
+def createChanges(cl, dateOverride):
+    """
+    Generates a dict that can be later turned into a formatted embed
+    """
+    date_cl = cl.get(dateOverride.date())
+    changes = {}
+    numCh = 0
+    for author, tags in date_cl.items():
+        numCh += 1
+        if not author in changes.keys():
+            changes.update({author : {}})
+        for tag in tags:
+            ch = change_to_tuples(tag)
+            if ch[0] in changes.get(author).keys():
+                changes[author][ch[0]].append(ch[1])
+            else:
+                changes[author].update({ch[0] : [ch[1]]})
+    return changes, numCh
+
+def main():
+    when = ""
+    how = False
+    if len(when):
+        how = True
+    print(when, len(when), how)
+
+if __name__ == "__main__":
+    main()
