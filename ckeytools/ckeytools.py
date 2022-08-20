c310e024df1924ef4a928d7f25f5baf3c0eb4187
BongaTheProto <93835010+BongaTheProto@users.noreply.github.com>
2022-08-20 00:54:22 -0500

    Update ckeytools.py

--- ckeytools/ckeytools.py
+++ ckeytools/ckeytools.py
@@ -290,11 +290,10 @@ class CkeyTools(BaseCog):
 tier_1 = [{tier1}]
 tier_2 = [{tier2}]
 tier_3 = [{tier3}]
-
 """.format(tier1=", ".join(["\"{}\"".format(c) for c in tier1]), \
     tier2=", ".join(["\"{}\"".format(c) for c in tier2]), \
     tier3=", ".join(["\"{}\"".format(c) for c in tier3]))
-            donatorfile.write(tomlkit.dumps(tomlkit.loads(new_text)).strip())
+            donatorfile.write(tomlkit.dumps(tomlkit.loads(new_text.strip())))
     
     #Functions to get cogs and info from the cogs
     async def get_tgdb_prefix(self, guild):
