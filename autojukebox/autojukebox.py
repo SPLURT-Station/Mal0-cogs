da1386d75aaed274506958bc87d8dc0bea432eb1
BongaTheProto <93835010+BongaTheProto@users.noreply.github.com>
2023-07-15 02:33:57 -0500

    Update autojukebox.py

--- autojukebox/autojukebox.py
+++ autojukebox/autojukebox.py
@@ -167,7 +167,7 @@ class AutoJukebox(commands.Cog):
         await self.config.custom("JUKEBOX_SUGGESTION", ctx.guild.id, suggestion).finished.set(True)
         await self.config.custom("JUKEBOX_SUGGESTION", ctx.guild.id, suggestion).approved.set(True)
         
-        await oldmsg.add_reaction(discord.utils.get(ctx.guild.emojis, name="musical_note"))
+        await oldmsg.add_reaction("musical_note")
         await ctx.tick()
         
     @commands.command(name="jukereject")
@@ -210,7 +210,7 @@ class AutoJukebox(commands.Cog):
         await self.config.custom("JUKEBOX_SUGGESTION", ctx.guild.id, suggestion).finished.set(True)
         await self.config.custom("JUKEBOX_SUGGESTION", ctx.guild.id, suggestion).approved.set(True)
         
-        await oldmsg.add_reaction(discord.utils.get(ctx.guild.emojis, name="x"))
+        await oldmsg.add_reaction("x")
         await ctx.tick()
         
     @commands.group()
